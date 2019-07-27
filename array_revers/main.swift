a87418828a9d33c399e837865320166b18350a45
savoptik <sav70792@gmail.com>
2019-07-27 16:31:31 +0300

    Описан реверс массива без использования системных функций двумя способами

--- array_revers/main.swift
+++ array_revers/main.swift
@@ -8,5 +8,21 @@
 
 import Foundation
 
-print("Hello, World!")
-
+// исходный массив
+var array: [Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+// первый вариант реверса с сохранением исходного массива
+var arrayReverss: [Int] = [] // массив для сохранения реверса
+var index = array.count - 1 // получение последнего элемента
+// перебираем элементы исходного массива с конца
+while index >= 0 {
+    arrayReverss.append(array[index])
+    index -= 1
+}
+print("Исходный массив \(array)")
+print("Реверс \(arrayReverss)")
+// реверс с уничтожением исходного массива
+arrayReverss.removeAll() // отчищаем массив
+while array.count > 0 {
+    arrayReverss.append(array.removeLast())
+}
+print("Сново реверс \(arrayReverss)")
