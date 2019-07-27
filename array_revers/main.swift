d7a6f86d3c01d550746304b7790eefaa60ae6c4e
savoptik <sav70792@gmail.com>
2019-07-27 16:46:49 +0300

    Добавлен вариант без использования второго массива

--- array_revers/main.swift
+++ array_revers/main.swift
@@ -10,9 +10,18 @@ import Foundation
 
 // исходный массив
 var array: [Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-// первый вариант реверса с сохранением исходного массива
+// разворот без использования вспомогательного массива
+var index = array.count - 1 // получение индекса последнего элемента
+print("исходный массив \(array)")
+while index >= 0 {
+    array.append(array[index])
+    array.remove(at: index)
+    index -= 1
+}
+print("реверс \(array)")
+// вариант реверса с сохранением исходного массива
 var arrayReverss: [Int] = [] // массив для сохранения реверса
-var index = array.count - 1 // получение последнего элемента
+index = array.count - 1 // получение последнего элемента
 // перебираем элементы исходного массива с конца
 while index >= 0 {
     arrayReverss.append(array[index])
