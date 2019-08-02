a0489622cee024486d38985b542680b49eb9672f
savoptik <sav70792@gmail.com>
2019-08-02 23:23:13 +0300

    Добавленно самое эффективное решение

--- array_revers/main.swift
+++ array_revers/main.swift
@@ -10,12 +10,21 @@ import Foundation
 
 // исходный массив
 var array: [Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-// разворот без использования вспомогательного массива
-var index = array.count - 1 // получение индекса последнего элемента
+// самый эффективный реверс без распараллеливания
+print("исходный массив \(array)")
+var index = 0
+while index < array.count / 2 {
+    let tmp = array[index]
+    array[index] = array[array.count - 1 - index]
+    array[array.count - 1 - index] = tmp
+    index += 1
+}
+print("реверс \(array)")
+// разворот без использования вспомогательного массива и дополнительных переменных
+index = array.count - 1 // получение индекса последнего элемента
 print("исходный массив \(array)")
 while index >= 0 {
-    array.append(array[index])
-    array.remove(at: index)
+    array.append(array.remove(at: index))
     index -= 1
 }
 print("реверс \(array)")
